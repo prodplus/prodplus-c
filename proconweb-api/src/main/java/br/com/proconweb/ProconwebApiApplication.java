5c2acb59abaf8e176f1ff49ee6236f04988fb69c
Marlon F Garcia <marlon_garcia@outlook.com>
2022-07-19 13:06:48 -0300

    primeiro commit

---
+++ proconweb-api/src/main/java/br/com/proconweb/ProconwebApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.proconweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProconwebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProconwebApiApplication.class, args);
+	}
+
+}
---
+++ proconweb-api/src/test/java/br/com/proconweb/ProconwebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.proconweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProconwebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
